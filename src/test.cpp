9ab1ee3fcb255d716168183580a94907c5a279f6
amartinram <al.martin14@proton.me>
2026-04-15 10:54:41 +0200

    Test updated

--- src/test.cpp
+++ src/test.cpp
@@ -3,14 +3,30 @@
 #include <ArduinoBLE.h>
 #include "Nicla_System.h" 
 
-constexpr char myUserID[] = "User_01"; 
-constexpr int DUMP_DAY = 10;        
-constexpr long MINUTE_INTERVAL = 10000; 
+constexpr int DUMP_DAY = 30; 
+constexpr int MAX_DAYS_TO_STORE = 2; 
+constexpr int MAX_BUFFER = DUMP_DAY * MAX_DAYS_TO_STORE; 
+constexpr long MINUTE_INTERVAL = 1000; 
 
 BLEService stepService("00001814-0000-1000-8000-00805f9b34fb"); 
-BLEStringCharacteristic logChar("00002a37-0000-1000-8000-00805f9b34fb", BLERead | BLENotify, 512); 
+BLECharacteristic logCharacteristic("00002a37-0000-1000-8000-00805f9b34fb", BLERead | BLENotify, 512);
 Sensor stepCounter(SENSOR_ID_STC);
 
+static unsigned long previousMillis = 0;
+static uint32_t lastTotalSteps = 0;
+static uint8_t dailyLog[MAX_BUFFER]; 
+static int currentMinuteIndex = 0;
+static bool hasPendingData = false;
+
+enum StateMachine {IDLE, SEND_HEADER, SEND_DATA};
+StateMachine stateMachine = IDLE;
+unsigned long lastBleTime = 0;
+int currentSendIndex = 0;
+int minutesToSend = 0;
+uint32_t totalStepsTaken = 0;
+
+constexpr unsigned long STATE_MACHINE_TIMER = 10000;
+BLEDevice central;
 
 void setup() {
   nicla::begin(); 
@@ -18,71 +34,115 @@ void setup() {
   stepCounter.begin();
   
   if (!BLE.begin()) {
-    while (1); 
+    while (1);
   }
   
   BLE.setLocalName("Nicla_Steps"); 
   BLE.setAdvertisedService(stepService);
-  stepService.addCharacteristic(logChar);
+  stepService.addCharacteristic(logCharacteristic);
   BLE.addService(stepService);
   
   BLE.advertise(); 
 }
 
-
 void loop() {
-  static unsigned long previousMillis = 0;
-  static uint32_t lastTotalSteps = 0;
-  static uint16_t dailyLog[DUMP_DAY]; 
-  static int currentMinuteIndex = 0;
-  static bool hasPendingData = false;
-
+  unsigned long now = millis();
 
   BHY2.update(); 
+  BLE.poll();
 
-  if (millis() - previousMillis >= MINUTE_INTERVAL) {
-    previousMillis = millis();
+  if (now - previousMillis >= MINUTE_INTERVAL) {
+    previousMillis += MINUTE_INTERVAL; 
     
     uint32_t currentTotalSteps = stepCounter.value();
-    uint16_t stepsThisMinute = (currentTotalSteps >= lastTotalSteps) ? (currentTotalSteps - lastTotalSteps) : 0;
+    uint32_t stepsDiff = (currentTotalSteps >= lastTotalSteps) ? (currentTotalSteps - lastTotalSteps) : 0;
+    
+    uint8_t stepsThisMinute = (stepsDiff > 255) ? 255 : (uint8_t)stepsDiff; 
     lastTotalSteps = currentTotalSteps;
     
-    if (currentMinuteIndex < DUMP_DAY) {
-      dailyLog[currentMinuteIndex] = stepsThisMinute;
-      currentMinuteIndex++;
-    } else {
+    if (currentMinuteIndex < MAX_BUFFER) {
+      dailyLog[currentMinuteIndex++] = stepsThisMinute;
+    } /*else {
+      memmove(dailyLog, dailyLog + 1, MAX_BUFFER - 1);
+      dailyLog[MAX_BUFFER - 1] = stepsThisMinute;
+    }*/
+
+    if (currentMinuteIndex >= DUMP_DAY) {
       hasPendingData = true;
     }
   }
 
-  if (hasPendingData) {
-    BLEDevice central = BLE.central();
+  if (!central || !central.connected()) {
+      central = BLE.central();
+  }
+  
+  if (hasPendingData && stateMachine == IDLE && central && central.connected() && logCharacteristic.subscribed()) {
+    stateMachine = SEND_HEADER;
+    lastBleTime = now;
+    currentSendIndex = 0;
+    minutesToSend = currentMinuteIndex; 
+  }
+
+  if (stateMachine != IDLE) {
     
-    if (central && central.connected() && logChar.subscribed()) {
-      delay(500); 
-      
-      logChar.writeValue("START");
-      delay(100); 
-      
-      String packet = "";
-      uint32_t totalForDay = 0;
+    if (now - lastBleTime > STATE_MACHINE_TIMER) {
+      stateMachine = IDLE;
+    }
+    else if (!central || !central.connected() || !logCharacteristic.subscribed()) {
+      stateMachine = IDLE; 
+    }
+    else if (now - lastBleTime >= 40) {
+      lastBleTime = now;
 
-      for (int i = 0; i < currentMinuteIndex; i++) { 
-        packet += String(dailyLog[i]) + ","; 
-        totalForDay += dailyLog[i];
+      switch (stateMachine) {
+        
+        case SEND_HEADER: {
+          totalStepsTaken = 0;
+          for (int i = 0; i < minutesToSend; i++) { 
+            totalStepsTaken += dailyLog[i]; 
+          }
 
-        if ((i + 1) % 40 == 0 || i == currentMinuteIndex - 1) {
-          logChar.writeValue(packet);
-          delay(100); 
-          packet = "";
+          uint8_t header[9];
+          //First two bytes preamble to mark the start of a transaction
+          header[0] = 0xAA; 
+          header[1] = 0xBB; 
+          header[2] = (minutesToSend >> 8) & 0xFF;
+          header[3] = minutesToSend & 0xFF;
+          header[4] = (totalStepsTaken >> 24) & 0xFF;
+          header[5] = (totalStepsTaken >> 16) & 0xFF;
+          header[6] = (totalStepsTaken >> 8) & 0xFF;
+          header[7] = totalStepsTaken & 0xFF;
+          header[8] = nicla::getBatteryVoltagePercentage();
+
+          logCharacteristic.writeValue(header, 9);
+          stateMachine = SEND_DATA;
+          break;
         }
+
+        case SEND_DATA: {
+          int bytesRemaining = minutesToSend - currentSendIndex;
+          if (bytesRemaining > 0) {
+            int chunkSize = (bytesRemaining > 244) ? 244 : bytesRemaining;
+            uint8_t buffer[244];
+            
+            memcpy(buffer, dailyLog + currentSendIndex, chunkSize);
+            
+            logCharacteristic.writeValue(buffer, chunkSize);
+            currentSendIndex += chunkSize;
+          } else {
+            memmove(dailyLog, dailyLog + minutesToSend, currentMinuteIndex - minutesToSend);
+            currentMinuteIndex -= minutesToSend;
+            
+            hasPendingData = false;
+            stateMachine = IDLE; 
+          }
+          break;
+        }
+        
+        case IDLE:
+        default:
+          break;
       }
-      
-      delay(200); 
-      logChar.writeValue("END:" + String(totalForDay));
-      
-      currentMinuteIndex = 0;
-      hasPendingData = false;
     }
   }
 }
