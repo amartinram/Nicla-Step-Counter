5743302823450936f83a288ab8a74c885450876f
amartinram <al.martin14@proton.me>
2026-05-06 16:24:55 +0200

    folders

--- include/Config.h
+++ include/Config.h
@@ -2,10 +2,10 @@
 #define CONFIG_H
 
 namespace Config {
-    constexpr int DUMP_DAY = 1440; 
+    constexpr int DUMP_DAY = 30; 
     constexpr int MAX_DAYS_TO_STORE = 2; 
     constexpr int MAX_BUFFER = DUMP_DAY * MAX_DAYS_TO_STORE; 
-    constexpr long MINUTE_INTERVAL = 60000;
+    constexpr long MINUTE_INTERVAL =1000;
 }
 
 #endif
